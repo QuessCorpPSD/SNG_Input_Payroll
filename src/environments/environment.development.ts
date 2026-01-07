bd3d82d3ae1bbc94666ce31c36a7dbd43f7ff9a0
Masethungr <Masethung.R@quesscorp.com>
2026-01-07 19:04:34 +0530

    code changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl:"http://localhost:59430/api/"
+     apiUrl:"http://localhost:59430/api/"
     //apiUrl:"https://localhost:50092/api/"
     //apiUrl:"https://localhost:59116/api/"
     //  apiUrl: "https://qsgerpapi.quesscorp.com/api/"
