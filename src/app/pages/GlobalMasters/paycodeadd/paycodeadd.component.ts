30a00c76ab3c995abf87b2e915bb187e22b0ce89
helen10037661 <helen.agustina@quesscorp.com>
2026-01-13 13:40:45 +0530

    paycode page modification

--- src/app/pages/GlobalMasters/paycodeadd/paycodeadd.component.ts
+++ src/app/pages/GlobalMasters/paycodeadd/paycodeadd.component.ts
@@ -4,12 +4,15 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
 import { MatCard, MatCardModule } from '@angular/material/card';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { PaycodeserviceService } from '../../../Service/GlobalMasters/paycodeservice.service';
 import { IPAycodeService } from '../../../Repository/GlobalMasters/Ipaycode.service';
+import { number } from 'echarts';
+import { EncryptionService } from '../../../Shared/encryption.service';
+import { SessionStorageService } from '../../../Shared/SessionStorageService';
 
 export const Paycode_TOKEN = new InjectionToken<IPAycodeService>('Paycode_TOKEN');
 @Component({
@@ -37,11 +40,19 @@ export const Paycode_TOKEN = new InjectionToken<IPAycodeService>('Paycode_TOKEN'
 export class PaycodeaddComponent {
   pagetype: any;
   AddPaycodeform!: FormGroup;
+  paytype: any;
+  isEditMode: boolean = false;
+  editingRowId: number | null = null;
+  userdetail: any;
 
-  constructor(@Inject(Paycode_TOKEN) private payCode: PaycodeserviceService, private dialogRef: MatDialogRef<PaycodeaddComponent>,) { }
+  constructor(@Inject(Paycode_TOKEN) private payCode: PaycodeserviceService, private dialogRef: MatDialogRef<PaycodeaddComponent>, @Inject(MAT_DIALOG_DATA) public editData: any, private _decrypt: EncryptionService, private _sessionStoreage: SessionStorageService,) { }
 
   ngOnInit(): void {
+    const userdetail = this._sessionStoreage.getItem('UserProfile');
+    this.userdetail = JSON.parse(this._decrypt.decrypt(userdetail!));
+
     this.BindPageType();
+    this.BindPayType();
     this.AddPaycodeform = new FormGroup({
       PayCode: new FormControl('', Validators.required),
       Description: new FormControl(''),
@@ -59,6 +70,16 @@ export class PaycodeaddComponent {
       MarginalTax: new FormControl(''),
       PayType: new FormControl('', Validators.required),
     });
+    if (this.editData?.mode?.toLowerCase() === 'edit') {
+      this.isEditMode = true;
+      this.patchEditData();
+    }
+  }
+
+  BindPayType() {
+    this.payCode.GetPayType().subscribe({
+      next: res => { this.paytype = res.Data }
+    });
   }
 
   BindPageType() {
@@ -67,60 +88,74 @@ export class PaycodeaddComponent {
     });
   }
 
+  patchEditData() {
+    const row = this.editData.row;
+    // PATCH FORM VALUES
+    this.AddPaycodeform.patchValue({
+      PayCode: row.Paycode_Code,
+      Description: row.Description,
+      PrintAs: row.Print_As,
+      LOPApplicable: row.Is_LOP_Applicable ? '1' : '0',
+      PFApplicable: row.Is_PF_Applicable ? '1' : '0',
+      ESIApplicable: row.Is_ESI_Applicable ? '1' : '0',
+      PTApplicable: row.Is_PT_Applicable ? '1' : '0',
+      Taxable: row.IsTaxable ? '1' : '0',
+      ProjectTax: row.IsProjectTax ? '1' : '0',
+      MarginalTax: row.IsMarginalTax ? '1' : '0',
+      PageType: row.Page_Type,
+      GLAccountNumber: row.Account_Number,
+      PostingKey: row.Account_Number,
+      PayCodeType: row.PayCode_Type ? '1' : '0',
+      PayType: row.PayType_Id
+    });
+    this.AddPaycodeform.get('PayCode')?.disable();
+  }
+
   onSavePayCode(): void {
     if (this.AddPaycodeform.invalid) {
-      console.warn('Form invalid. Please fill all required fields.');
-
       this.AddPaycodeform.markAllAsTouched();
-
       alert("Please enter all required fields.")
       return;
     }
 
     const formValue = this.AddPaycodeform.getRawValue();
-    const boolToNum = (val: any) => (val ? 1 : 0);
-
-    // Construct XML string
-    const xmlDetails = `
-<Root>
-  <Row
-    PayCode="${formValue.PayCode || ''}"
-    Description="${formValue.Description || ''}"
-    PrintAs="${formValue.PrintAs || ''}"
-    PayType_Id="${formValue.PayType || 0}"
-    Is_Taxable="${boolToNum(formValue.Taxable)}"
-    Is_ProjectTax="${boolToNum(formValue.ProjectTax)}"
-    Is_MarginalTax="${boolToNum(formValue.MarginalTax)}"
-    PayCode_Type="${formValue.PayCodeType || 0}"
-    Is_LOP_Applicable="${boolToNum(formValue.LOPApplicable)}"
-    Is_PF_Applicable="${boolToNum(formValue.PFApplicable)}"
-    Is_ESI_Applicable="${boolToNum(formValue.ESIApplicable)}"
-    Is_PT_Applicable="${boolToNum(formValue.PTApplicable)}"
-    Page_Type="${formValue.PageType || 0}"
-    Account_Number="${formValue.GLAccountNumber || ''}"
-    Posting_Key="${formValue.PostingKey || ''}"
-  />
-</Root>
-`.trim();
 
     const payload = {
-      strXmlDetails: xmlDetails,
       mode: 'Add',
-      userId: 3
+      userId: this.userdetail.user_Id,
+      detail: {
+        Paycode_Id: 0,
+        Paycode_Code: formValue.PayCode ?? '',
+        Description: formValue.Description ?? '',
+        Print_As: formValue.PrintAs ?? '',
+        PayType_Id: Number(formValue.PayType),
+        IsTaxable: formValue.Taxable == 1,
+        IsProjectTax: formValue.ProjectTax == 1,
+        IsMarginalTax: formValue.MarginalTax == 1,
+        Paycode_Type: formValue.PayCodeType == 1,
+        Is_LOP_Applicable: formValue.LOPApplicable == 1,
+        Is_PF_Applicable: formValue.PFApplicable == 1,
+        Is_ESI_Applicable: formValue.ESIApplicable == 1,
+        Is_PT_Applicable: formValue.PTApplicable == 1,
+        Page_Type: Number(formValue.PageType) || 0,
+        Account_Number: Number(formValue.GLAccountNumber) || 0,
+        Posting_key: Number(formValue.PostingKey) || 0,
+        Tax_Type_ID: Number(formValue.TaxTypeId) || 0
+      }
+
     };
-
-
- 
+    console.log('payload', JSON.stringify(payload));
     this.payCode.CreatePayCode(payload).subscribe({
       next: (res: any) => {
-        if (res?.StatusCode === 200 && res?.Data?.statusCode === '200') {
-          alert("PayCode created successfully.")
-          this.dialogRef?.close();
+        if (res?.StatusCode === 200) {
+          alert(res.Data?.data?.Table0[0]?.Error_Message);
+          this.dialogRef.close('updated');
         } else {
           const message =
             res?.Data?.message ||
             res?.Message ||
             'PayCode creation failed. Please try again.';
+          alert(message);
         }
       },
       error: (err) => {
@@ -128,6 +163,61 @@ export class PaycodeaddComponent {
       }
     });
   }
+
+  onUpdatePayCode(): void {
+    if (this.AddPaycodeform.invalid) {
+      this.AddPaycodeform.markAllAsTouched();
+      alert("Please enter all required fields.");
+      return;
+    }
+
+    const formValue = this.AddPaycodeform.getRawValue();
+
+    const payload = {
+      mode: 'Edit',
+      userId: this.userdetail.user_Id,
+      detail: {
+        Paycode_Id: this.editData.row.Paycode_Id,
+        Paycode_Code: formValue.PayCode ?? '',
+        Description: formValue.Description ?? '',
+        Print_As: formValue.PrintAs ?? '',
+        PayType_Id: Number(formValue.PayType),
+        IsTaxable: formValue.Taxable == 1,
+        IsProjectTax: formValue.ProjectTax == 1,
+        IsMarginalTax: formValue.MarginalTax == 1,
+        Paycode_Type: formValue.PayCodeType == 1,
+        Is_LOP_Applicable: formValue.LOPApplicable == 1,
+        Is_PF_Applicable: formValue.PFApplicable == 1,
+        Is_ESI_Applicable: formValue.ESIApplicable == 1,
+        Is_PT_Applicable: formValue.PTApplicable == 1,
+        Page_Type: Number(formValue.PageType) || 0,
+        Account_Number: Number(formValue.GLAccountNumber) || 0,
+        Posting_key: Number(formValue.PostingKey) || 0,
+        Tax_Type_ID: Number(formValue.TaxTypeId) || 0
+      }
+    };
+    console.log('payload', JSON.stringify(payload));
+
+    this.payCode.CreatePayCode(payload).subscribe({
+      next: (res: any) => {
+        if (res?.StatusCode === 200) {
+          alert(res.Data?.data?.Table0[0]?.Error_Message);
+          this.dialogRef.close('updated');
+        } else {
+          alert(
+            res?.Data?.message ||
+            res?.Message ||
+            'PayCode update failed.'
+          );
+        }
+      },
+      error: (err) => {
+        console.error('Error while updating PayCode:', err);
+        alert('Something went wrong while updating.');
+      }
+    });
+  }
+
 
 
   onClose() {
