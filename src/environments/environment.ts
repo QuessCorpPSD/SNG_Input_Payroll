7d90df4ddc25064de5a8a1721f8f87c20a1bee9f
monika.ramesh@quesscorp.com <monika.ramesh@quesscorp.com>
2026-08-13 17:24:41 +0530

    env changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     //apiUrl:"https://localhost:7087/api/"
-    apiUrl:"https://localhost:57619/api/"
-    //apiUrl: "https://qsgerpapi.quesscorp.com/api/"
+    //apiUrl:"https://localhost:57619/api/"
+    apiUrl: "https://qsgerpapi.quesscorp.com/api/"
 };
